cae3be253a4872c0283465f59efabd33fd8004ca
tnarik <tnarik@gmail.com>
2013-04-27 20:55:12 +0100

    Multistage deployment successful, with different DBs. The recipes could be improved and packaged, but this is still a test anyway.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,25 +1,22 @@
 require 'bundler/capistrano'
+require 'capistrano/ext/multistage'
+
+set :stages, %w(production staging)
+set :default_stage, "staging"
+
 
 set :application, "hostedupped"
+
 
 set :scm, :git
 set :repository,  "git@github.com:tnarik/upped_rails_tutorial.git"
 set :scm_passphrase, ""
 
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "nonmachinepeople.net"                          # Your HTTP server, Apache/etc
-role :app, "nonmachinepeople.net"                          # This may be the same as your `Web` server
-role :db,  "nonmachinepeople.net", :primary => true # This is where Rails migrations will run
-
-set :deploy_to, "/home/tnarik/ror/#{application}"
 set :use_sudo, false
 
-set :user, "tnarik"  # The server's user for deploys
 set :ssh_options, { :forward_agent => true }
 set :git_shallow_clone, 1
-
 
 ## RVM and capistrano
 set :rvm_require_role, :app
@@ -30,13 +27,6 @@ before 'deploy:setup', 'rvm:install_rvm'   # install RVM
 before 'deploy:setup', 'rvm:install_ruby'  # install Ruby and create gemset, OR:
 #before 'deploy:setup', 'rvm:create_gemset' # only create gemset
 require "rvm/capistrano"
-
-namespace :deploy do
-  desc "Restart server"
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} touch #{File.join(current_release,'tmp','restart.txt')}"
-  end
-end
 
 namespace :rvm do
   task :trust_rvmrc do
@@ -63,24 +53,23 @@ namespace :deploy do
         production:
           adapter: postgresql
           encoding: unicode
-          database: upped
+          database: upped_#{stage}
           host: localhost
           username: upped
           password: #{Capistrano::CLI.password_prompt("Enter database password: ")}
       EOF
 
       config = ERB.new(template)
-
-      run "mkdir -p #{shared_path}/config"
-      put config.result(binding), "#{shared_path}/config/database.yml"
+      run "mkdir -p #{File.join(shared_path,'config')}"
+      put config.result(binding), "#{File.join(shared_path,'config','database.yml')}" 
     end
 
     desc <<-DESC
       [internal] Updates the symlink for database.yml file to the just deployed release.
     DESC
     task :symlink, :except => { :no_release => true } do
-      run "rm #{release_path}/config/database.yml"
-      run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+      run "rm #{File.join(current_release,'config','database.yml')}"
+      run "ln -nfs #{File.join(shared_path,'config','database.yml')} #{File.join(current_release,'config','database.yml')}" 
     end
 
   end
@@ -97,11 +86,10 @@ after "deploy:finalize_update", "deploy:db:symlink"
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
---
+++ config/deploy/staging.rb
@@ -0,0 +1,7 @@
+role :web, "nonmachinepeople.net"                          # Your HTTP server, Apache/etc
+role :app, "nonmachinepeople.net"                          # This may be the same as your `Web` server
+role :db,  "nonmachinepeople.net", :primary => true # This is where Rails migrations will run
+
+set :deploy_to, "/home/tnarik/ror/staged_#{application}"
+
+set :user, "tnarik"  # The server's user for deploys
