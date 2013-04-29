a129534356e0bf83c475b0aa6b40047e867aed40
tnarik <tnarik@gmail.com>
2013-04-29 21:28:12 +0100

    config.action_mailer.default_url_options is created, doesn't exist in environment configurations.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,5 +65,5 @@ Upped::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_mailer.default_url_options[:host] = "upped.me"
+  config.action_mailer.default_url_options = { host: "upped.me" }
 end
