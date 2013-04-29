795caa34c492928198c6966fe4828ee3e17eda0a
tnarik <tnarik@gmail.com>
2013-04-29 09:02:54 +0100

    Fixing issues with the config.yml file

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -5,7 +5,7 @@ ActionMailer::Base.smtp_settings = {
   :port                 => 587,
   :domain               => "gmail.com",
   :user_name            => "tnarik@gmail.com",
-  :password             => APP_CONFIG['smtp_password'] || 'kk',
+  :password             => 'kk', #APP_CONFIG['smtp_password'] || 
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
