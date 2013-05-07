3251b59b93eb4516cd678bc4e3a7a99a94bedf64
tnarik <tnarik@gmail.com>
2013-05-07 08:33:30 +0100

    testing Gabba to see what it can do

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,20 @@
 module ApplicationHelper
-    # Returns the full title on a per-page basis.
-    def full_title(page_title)
-        base_title = "Tnarik's App"
-        if page_title.empty?
-            base_title
-        else
-            "#{base_title} | #{page_title}"
-        end
+  # Returns the full title on a per-page basis.
+  def full_title(page_title)
+    base_title = "Tnarik's App"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
     end
+  end
 
-    def ga_track
-      Gabba::Gabba.new("UA-40707807-1", "upped.me").event("category_yy", "action_nn")
-    end
+  def ga_track
+    gabba = Gabba::Gabba.new("UA-40707807-1", "upped.me")
+    gabba.referer(request.env['HTTP_REFERER']) if request
+    gabba.ip(request.env["REMOTE_ADDR"]) if request
+    gabba.page_view("title", "path")
+    p gabba
+    gabba.event("category_yy", "action_nn")
+  end
 end
